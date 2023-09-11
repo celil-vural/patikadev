eb2a628d076854f4a089d877fa0e39d4b65813f1
celil-vural <celilvural.work@gmail.com>
2023-09-11 22:45:39 +0300

    Reverse string

---
+++ ReverseString/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ReverseString
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Bir string ifade girin: ");
+            string input = Console.ReadLine() ?? "";
+            string reversed = new string(input.Reverse().ToArray());
+            Console.WriteLine("Sonuç: " + reversed);
+            Console.ReadLine();
+        }
+    }
+
+}
