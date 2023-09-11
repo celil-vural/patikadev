136015f8fac64cd42517749935264d3a66fb8c4f
celil-vural <celilvural.work@gmail.com>
2023-09-11 22:25:03 +0300

    DrawATriangle

---
+++ DrawATriangle/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace DrawATriangle
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of rows: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+            DrawATriangle(rows);
+            Console.ReadKey();
+        }
+        public static void DrawATriangle(int rows)
+        {
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
